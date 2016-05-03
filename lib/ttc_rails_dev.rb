7af617e33b41356738725fb4ca97a62e6b8ee2fa
Rafael Polo <rafael@tacticaltech.org>
2016-05-03 10:42:32 +0200

    require deploy_helpers

--- lib/ttc_rails_dev.rb
+++ lib/ttc_rails_dev.rb
@@ -14,6 +14,7 @@ module TtcRailsDev
     require 'sqlite3'
     require 'binding_of_caller'
     require 'better_errors'
+    require 'deploy_helpers'
 
     config.autoload_paths << File.expand_path('../../', __FILE__)
 
